4c2e6e62ae7c1337a606a0078497cbc5c9f6e2d6
vjvjvjvjvjv <116143761+vjvjvjvjvjv@users.noreply.github.com>
2023-04-07 17:56:47 -0400

    Add .js file

---
+++ gallery.js
@@ -0,0 +1,17 @@
+async function loadImages() {
+    const response = await fetch('https://api.github.com/repos/benjooom/HCI-Final/contents/images');
+    const data = await response.json();
+    const imagesContainer = document.getElementById('images-container');
+
+    data.forEach(image => {
+      if (image.type === 'file' && image.path.startsWith('images/')) {
+        const img = document.createElement('img');
+        img.src = image.download_url;
+        img.alt = image.name;
+        img.width = 200;
+        imagesContainer.appendChild(img);
+      }
+    });
+}
+
+loadImages();
