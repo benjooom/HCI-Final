63249a6d227cb5d133caabc43f9f676d6e13b55f
vjvjvjvjvjv <116143761+vjvjvjvjvjv@users.noreply.github.com>
2023-04-08 16:41:45 -0400

    1

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,17 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  loadImages();
+});
+
 async function loadImages() {
-    const response = await fetch('https://api.github.com/repos/benjooom/HCI-Final/contents/gallery/images/');
-    const data = await response.json();
-    const imagesContainer = document.getElementById('images-container');
+  const response = await fetch('https://api.github.com/repos/benjooom/HCI-Final/contents/gallery/images/');
+  const data = await response.json();
+  const imagesContainer = document.getElementById('images-container');
 
-    data.forEach(image => {
-      if (image.type === 'file' && image.path.startsWith('images/')) {
-        const img = document.createElement('img');
-        img.src = image.download_url;
-        img.alt = image.name;
-        img.width = 200;
-        imagesContainer.appendChild(img);
-      }
-    });
+  data.forEach(image => {
+    if (image.type === 'file' && image.path.startsWith('images/')) {
+      const img = document.createElement('img');
+      img.src = image.download_url;
+      img.alt = image.name;
+      img.width = 200;
+      imagesContainer.appendChild(img);
+    }
+  });
 }
-
-loadImages();
