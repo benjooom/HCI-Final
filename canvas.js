df1e62e7aa4e8fd9654d73d5b0b921269dc9d4d3
zjiao19 <zjiao19@outlook.com>
2023-04-17 01:48:23 -0400

    fine tune tracking

--- canvas.js
+++ canvas.js
@@ -1,17 +1,45 @@
 var host = "localhost:4444";
+// var host = "cpsc484-03.yale.internal:8888";
+
+var DEBUG = true;
+
+var stroke_weight = 4;
+var body_scale = 0.2;
+var speed_sensitivity = 80; // value up => less likely white 
+var col_pair = ["#84773b", "#2c1c99"];
+
+// used to control if the screen is paused.
+var is_paused = false;
+var prev = null;
+var paint_canvas;
+
+// For dev only. choose a draw with a col for 15 sec and change to another.
+var col = col_pair[0];
+setTimeout(() => {
+    col = col_pair[1];
+    trackingMgr.center = null;
+}, 15000);
+
+// pause demo
+// setTimeout(() => { is_paused = true; }, 5000);
+// setTimeout(() => { is_paused = false; prev = null; }, 10000);
+
+// save image demo
+// setTimeout(() => { saveCanvas(paint_canvas, 'myCanvas', 'jpg'); }, 30000); // save as file
+// setTimeout(() => { console.log(paint_canvas.elt.toDataURL()); }, 30000); // get base64 encode string
 
 $(document).ready(function () {
     trackingMgr.init();
-    twod.start();
+    if (DEBUG) {
+        twod.start();
+    }
 });
-
 
 /*
 Error:
 0: normal
 1: no people detected
 */
-
 var trackingMgr = {
     socket: null,
     joints: [],
@@ -28,28 +56,97 @@ var trackingMgr = {
     updateJoints: function (frame) {
         if (frame.people.length < 1) {
             trackingMgr.error = 1;
+            trackingMgr.center = null;
             return;
         }
         trackingMgr.error = 0;
 
         if (trackingMgr.center == null) {
             // init center if first frame
+            trackingMgr.bodyId = frame.people[0].body_id;
+            console.log(`Tracking ${trackingMgr.bodyId}`);
             trackingMgr.center = [
-                frame.people[0].joints[0].position.x,
-                frame.people[0].joints[0].position.y,
-                frame.people[0].joints[0].position.z
-            ];
-        }
-        for (var i = 0; i < frame.people[0].joints.length; i++) {
-            trackingMgr.joints[i] = [
-                (frame.people[0].joints[i].position.x - trackingMgr.center[0]) * -1 / 4,
-                (frame.people[0].joints[i].position.y - trackingMgr.center[1]) * -1 / 4,
-                (frame.people[0].joints[i].position.z - trackingMgr.center[2]) * -1 / 4,
+                // joints[2] is SPINE_CHEST
+                frame.people[0].joints[2].position.x,
+                frame.people[0].joints[2].position.y
             ];
         }
 
-    }
+        // query frame to find the people with body id
+        jsonata(`people[body_id=${trackingMgr.bodyId}]`).evaluate(frame).then((people) => {
+            if (people == null) {
+                // if prev body missing, re-init
+                trackingMgr.center = null;
+                return;
+            }
+            console.log(people);
+            for (var i = 0; i < people.joints.length; i++) {
+                trackingMgr.joints[i] = [
+                    (people.joints[i].position.x - trackingMgr.center[0]) * -1 * body_scale, // mirror
+                    (people.joints[i].position.y - trackingMgr.center[1]) * body_scale,
+                    (people.joints[i].position.z - trackingMgr.center[2]) * body_scale,
+                ];
+            }
+        });
+    },
+    isY: function (){
+        // TODO
+        return false;
+    },
 };
+
+
+function setup() {
+    paint_canvas = createCanvas(windowWidth, windowHeight);
+    // paint_canvas = createCanvas(800, 600);
+    paint_canvas.parent("canvas-container");
+    frameRate(5);
+    stroke("black");
+    strokeWeight(stroke_weight);
+    background('black');
+
+}
+
+function draw() {
+    if (is_paused) {
+        return;
+    }
+    var newData = trackingMgr.joints;
+    if (newData.length > 0) {
+
+        // // start chosen index only
+        // var chosen_joint_index = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+        // for (var j = 0; j < chosen_joint_index.length; j++) {
+        //     var i = chosen_joint_index[j];
+        // // end chosen index only
+        
+        // star all joints
+            for (var i = 0; i < newData.length; i++) {
+        // end all joints
+        
+            if (prev == null || prev[0] == null) {
+                point(newData[i][0] + (width / 2), newData[i][1] + (height / 2));
+            } else {
+                var d = dist(
+                    prev[i][0] + (width / 2), prev[i][1] + (height / 2),
+                    newData[i][0] + (width / 2), newData[i][1] + (height / 2)
+                );
+
+                stroke(
+                    map(d, 0, speed_sensitivity, red(color(col)) * 0.8, 255),
+                    map(d, 0, speed_sensitivity, green(color(col)) * 0.8, 255),
+                    map(d, 0, speed_sensitivity, blue(color(col)), 255),
+                    0.4 * 255
+                );
+                line(
+                    prev[i][0] + (width / 2), prev[i][1] + (height / 2),
+                    newData[i][0] + (width / 2), newData[i][1] + (height / 2)
+                );
+            }
+        }
+    }
+    prev = newData.slice(0);
+}
 
 var twod = {
     socket: null,
@@ -66,52 +163,3 @@ var twod = {
         $('.twod').attr("src", 'data:image/pnjpegg;base64,' + twod.src);
     }
 };
-
-
-function setup() {
-    let snakeCanvas = createCanvas(windowWidth / 2, windowHeight / 2);
-    snakeCanvas.parent("canvas-container");
-    frameRate(30);
-    stroke(255);
-    strokeWeight(10);
-    background('black');
-}
-var prev = null;
-
-var col = [0, 53, 107];
-
-setTimeout(() => {col = [208, 190, 8];}, 30000);
-
-
-function draw() {
-    var i = 0;
-    var newData = trackingMgr.joints;
-    var ind = [0, 9, 10, 13, 14];
-    // var newData = [[mouseX - width / 2, mouseY - height / 2]];
-    if (newData.length > 0) {
-        for (var j = 0; j < ind.length; j++) {
-            i = ind[j];
-            if (prev == null || prev[0] == null) {
-                point(newData[i][0] + (width / 2), newData[i][1] + (height / 2));
-            } else {
-                var d = dist(
-                    prev[i][0] + (width / 2), prev[i][1] + (height / 2),
-                    newData[i][0] + (width / 2), newData[i][1] + (height / 2)
-                );
-                
-                stroke(
-                    map(d, 0, 700, col[0] * 0.8, 255),
-                    map(d, 0, 700, col[1] * 0.8, 255),
-                    map(d, 0, 700, col[2], 255),
-                    0.4 * 255
-                );
-                line(
-                    prev[i][0] + (width / 2), prev[i][1] + (height / 2),
-                    newData[i][0] + (width / 2), newData[i][1] + (height / 2)
-                );
-            }
-        }
-    }
-    prev = newData.slice(0);
-    // point(mouseX, mouseY);
-}
